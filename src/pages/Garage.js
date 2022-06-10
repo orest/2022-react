b3c437a8370e93d5cd67bdcad99d29f2fa58cdaa
Orest Bootcamp <orest@proficiencyconsulting.com>
2022-06-09 22:09:55 -0400

    From another user, not Orest

--- src/pages/Garage.js
+++ src/pages/Garage.js
@@ -1,7 +1,9 @@
 import PageBody from "../components/UI/PageBody";
 
 const Garage = () => {
-  return <PageBody pageTitle="Garage"></PageBody>;
+  return <PageBody pageTitle="Garage">
+<h1>From another user</h1>
+  </PageBody>;
 };
 
 export default Garage;
